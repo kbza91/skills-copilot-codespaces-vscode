32712daba70a3a59a3186ef1f2b42a0d0659dd1e
Pablo Ibañez <54422862+kbza91@users.noreply.github.com>
2023-11-04 00:14:40 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,102 @@
+//Create web server with express
+const express = require('express');
+const app = express();
+
+//Create a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Listen to port 5000
+app.listen(5000);
+
+// Path: index.js
+//Import express library
+const express = require('express');
+const app = express();
+
+//Create a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Listen to port 5000
+app.listen(5000);
+
+// Path: index.js
+//Import express library
+const express = require('express');
+const app = express();
+
+//Create a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Listen to port 5000
+app.listen(5000);
+
+// Path: index.js
+//Import express library
+const express = require('express');
+const app = express();
+
+//Create a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Listen to port 5000
+app.listen(5000);
+
+// Path: index.js
+//Import express library
+const express = require('express');
+const app = express();
+
+//Create a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Listen to port 5000
+app.listen(5000);
+
+// Path: index.js
+//Import express library
+const express = require('express');
+const app = express();
+
+//Create a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Listen to port 5000
+app.listen(5000);
+
+// Path: index.js
+//Import express library
+const express = require('express');
+const app = express();
+
+//Create a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Listen to port 5000
+app.listen(5000);
+
+// Path: index.js
+//Import express library
+const express = require('express');
+const app = express();
+
+//Create a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Listen to port 5000
+app
